abd1f39f217e8eef29a3d73497f14d5a87d0212e
ikoinaris <g.koinaris@hotmail.com>
2020-11-12 14:35:27 +0100

    Product controller receive Requests from UI + MongoDB Connection string

--- src/main/java/com/ikoinaris/product/controller/ProductController.java
+++ src/main/java/com/ikoinaris/product/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.ikoinaris.product.controller;
 import com.ikoinaris.product.model.Product;
 import com.ikoinaris.product.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ public class ProductController {
     private ProductRepository productRepository;
 
     @GetMapping("/products")
+    @CrossOrigin(origins = "http://localhost:4200")
     public List<Product> getAllProducts() {
         return productRepository.findAll();
     }
