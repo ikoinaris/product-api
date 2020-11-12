39980fda64aecbfdff5eb3a0d262bce27b49ecb1
ikoinaris <g.koinaris@hotmail.com>
2020-11-12 12:38:01 +0100

    Product Controller created

---
+++ src/main/java/com/ikoinaris/product/controller/ProductController.java
@@ -0,0 +1,21 @@
+package com.ikoinaris.product.controller;
+
+import com.ikoinaris.product.model.Product;
+import com.ikoinaris.product.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping("/products")
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+}
